108622fb093730b7c30368c2d5086bcbc62c0b5b
parker <wallace.parker@hotmail.com>
2025-02-24 09:42:09 -0800

    updated URI for security

--- assignment02/app.js
+++ assignment02/app.js
@@ -1,30 +1,30 @@
+require('dotenv').config(); // Load the environment variables from the .env file
+
 const mongoose = require('mongoose');
 const express = require("express");
-const courseRoute = require ("./routes/course.route.js")
-const studentRoute = require ("./routes/student.route.js")
-const regiserRoute = require ("./routes/register.route.js")
+const courseRoute = require("./routes/course.route.js");
+const studentRoute = require("./routes/student.route.js");
+const regiserRoute = require("./routes/register.route.js");
 const app = express();
 const port = 3000;
-
-// const Course = require ('./models/course.model');
-// const Student = require ('./models/student.model');
 
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/courses", courseRoute)
-app.use("/api/students", studentRoute)
-app.use("/api/register", regiserRoute)
+app.use("/api/courses", courseRoute);
+app.use("/api/students", studentRoute);
+app.use("/api/register", regiserRoute);
 
+const dbURI = process.env.MONGO_URI; // Use the environment variable for the MongoDB URI
 
-mongoose.connect('mongodb+srv://Student05:Student05@home05.k7myv.mongodb.net/', {dbName: 'home05'})
-.then(() => {
-    console.log("Connected to the database!");
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-    });
-})
-.catch(() => {
-    console.log("Failed to connect to the database.");
-});
+mongoose.connect(dbURI, { dbName: 'home05' })
+  .then(() => {
+      console.log("Connected to the database!");
+      app.listen(port, () => {
+          console.log(`Example app listening on port ${port}`);
+      });
+  })
+  .catch(() => {
+      console.log("Failed to connect to the database.");
+  });
